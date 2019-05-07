932f39ac8fb92df562db3a8f16874246853a114c
Kushal Deb <kushaldeb.iiit@gmail.com>
2019-05-07 15:22:01 +0530

    updated move-1.py

--- move-1.py
+++ move-1.py
@@ -18,8 +18,8 @@ subdir_list = [0, 1]
 
 for j in tqdm(subdir_list):
     for i in dir_list:
-        src_path = '/home/kushaldeb/D/8th Semester/Breast-Cancer-Classification/Original-Dataset/'+str(i)+'/'+str(j)
-        dst_path = '/home/kushaldeb/D/8th Semester/Breast-Cancer-Classification/Dataset/'+str(j)
+        src_path = '/home/kushaldeb/D/8th-Semester/Breast-Cancer-Classification/Original-Dataset/'+str(i)+'/'+str(j)
+        dst_path = '/home/kushaldeb/D/8th-Semester/Breast-Cancer-Classification/Dataset/'+str(j)
         
         for file in glob.iglob(os.path.join(src_path, "*.png")):
             shutil.move(file, dst_path)
